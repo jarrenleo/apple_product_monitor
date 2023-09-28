10fcc05a653f8cc7fc43cd80712bb737fdd3b8ca
Jarren Leo <jarren.leoo@gmail.com>
2023-09-28 14:23:29 +0800

    commit

--- index.js
+++ index.js
@@ -12,33 +12,37 @@ class AppleProduct {
   async checkStock() {
     try {
       const response = await fetch(
-        `https://www.apple.com/sg/shop/pickup-message-recommendations?mts.0=regular&location=${this.zipcode}&product=${this.productId}`
+        `https://www.apple.com/sg/shop/fulfillment-messages?pl=true&mts.0=regular&mts.1=compact&parts.0=${this.productId}&location=${this.zipcode}`
       );
       if (!response.ok) throw Error("Stock check failed");
 
       const { body } = await response.json();
-      const modelIds = body.PickupMessage.stores[0].partsAvailability;
+      const modelAvailabiltity =
+        body.content.pickupMessage.stores[0].partsAvailability[this.productId];
 
-      if (modelIds.hasOwnProperty(this.productId)) this.sendMessage();
+      if (modelAvailabiltity.pickupDisplay === "available")
+        this.sendMessage(
+          modelAvailabiltity.messageTypes.regular.storePickupProductTitle
+        );
     } catch (e) {
       console.log(e.message);
     }
   }
 
-  async sendMessage() {
+  async sendMessage(productTitle) {
     await fetch(process.env.DiSCORD_WEBHOOK, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        content: "<@276649462166978560> iPhone 15 Pro restocked!",
+        content: `<@276649462166978560> ${productTitle} restocked!`,
       }),
     });
   }
 
   startMonitor() {
-    setInterval(() => this.checkStock(), 60000);
+    setInterval(() => this.checkStock(), 5000);
   }
 }
 
